c913f5542db7b39d2b4aec8f19df5bea600f5a29
yusptar <yusupvatar@gmail.com>
2022-08-31 09:00:16 +0700

    hasil praktikum 2

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 ## Praktikum 1: Menerapkan Gesture Detector
 ![Result](images/prak1.png)
+## Praktikum 2: Menerapkan Input Widget dan Forms
+![Result](images/prak2.png)
+## Praktikum 3: Menerapkan Custom Input dan FormField Widget
+![Result](images/prak3.png)
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_fundamental_part_3/basic_widgets/image_widget.dart';
+import 'basic_widgets/image_widget.dart';
+
+import 'basic_widgets/forms.dart';
 
 void main() {
   runApp(const MyApp());
@@ -46,14 +48,20 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: GestureDetector(
             onTap: _incrementCounter,
+            // onDoubleTap : _incrementCounter, // klik 2 kali
+            // onLongPress : _incrementCounter, // klik tahan
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                const MyImageWidget(),
+                // const MyImageWidget(),
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
                 Text(
                   '$_counter',
                   style: Theme.of(context).textTheme.headline4,
                 ),
+                const FormContoh(),
               ],
             )),
       ),
